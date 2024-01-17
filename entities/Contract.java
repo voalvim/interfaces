dac0293d80942e29aa49a0da531345ce78dd3ff6
Vitor Ornelas <ornelas.dev@gmail.com>
2024-01-17 16:08:36 -0300

    First commit

---
+++ entities/Contract.java
@@ -0,0 +1,48 @@
+package interfaces.entities;
+
+import java.util.Date;
+
+public class Contract {
+    private Integer number;
+    private Date date;
+    private Double totalValue;
+    private Installment installments;
+    public Contract() {
+    }
+    public Contract(Integer number, Date date, Double totalValue, Installment installments) {
+        this.number = number;
+        this.date = date;
+        this.totalValue = totalValue;
+        this.installments = installments;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Double getTotalValue() {
+        return totalValue;
+    }
+
+    public void setTotalValue(Double totalValue) {
+        this.totalValue = totalValue;
+    }
+
+    public Installment getInstallments() {
+        return installments;
+    }
+
+}
+

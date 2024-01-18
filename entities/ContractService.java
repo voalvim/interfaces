391d9af4fc5e70a4b37c63703cca74878a7177be
Vitor Ornelas <ornelas.dev@gmail.com>
2024-01-18 16:22:05 -0300

    Creating the structure and logic of the interfaces

---
+++ entities/ContractService.java
@@ -0,0 +1,8 @@
+package interfaces.entities;
+
+public class ContractService {
+    private OnlinePaymentService paymentService;
+    public void processContract(Contract contract, Integer months) {
+
+    }
+}

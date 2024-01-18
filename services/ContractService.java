44bbed4ddb2f02f84863304ec563284220f668d1
Vitor Ornelas <ornelas.dev@gmail.com>
2024-01-18 18:14:54 -0300

    Implementing ContractService main logic

---
+++ services/ContractService.java
@@ -0,0 +1,31 @@
+package interfaces.services;
+
+import interfaces.entities.Contract;
+import interfaces.entities.Installment;
+
+
+public class ContractService {
+    private OnlinePaymentService paymentService;
+
+    public ContractService(OnlinePaymentService paymentService) {
+        this.paymentService = paymentService;
+    }
+
+    public OnlinePaymentService getPaymentService() {
+        return paymentService;
+    }
+
+    public void setPaymentService(OnlinePaymentService paymentService) {
+        this.paymentService = paymentService;
+    }
+
+    public void processContract(Contract contract, Integer months) {
+        double monthlyInstallment = contract.getTotalValue() / months;
+        for (int i = 1; i <= months; i++) {
+            double interest = paymentService.interest(monthlyInstallment, i);
+            double fee = paymentService.paymentFee(interest + monthlyInstallment);
+            double total = interest + fee + monthlyInstallment;
+            contract.addInstallments(new Installment(contract.getDate().plusMonths(i),total));
+        }
+    }
+}

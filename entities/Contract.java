18e6bd879dd32129cce62150ec89db2daca0304a
Vitor Ornelas <ornelas.dev@gmail.com>
2024-01-18 17:12:24 -0300

    Refactoring ArrayList with List

--- entities/Contract.java
+++ entities/Contract.java
@@ -2,12 +2,13 @@ package interfaces.entities;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.List;
 
 public class Contract {
     private Integer number;
     private LocalDate date;
     private Double totalValue;
-    private ArrayList<Installment> installments = new ArrayList<>();
+    private List<Installment> installments = new ArrayList<>();
     public Contract() {
     }
     public Contract(Integer number, LocalDate date, Double totalValue) {
@@ -44,7 +45,7 @@ public class Contract {
         installments.add(installment);
     }
 
-    public ArrayList<Installment> getInstallments() {
+    public List<Installment> getInstallments() {
         return installments;
     }
 

139ee246c32ef2d3ad8296e4d83253e1045034a5
Vitor Ornelas <ornelas.dev@gmail.com>
2024-01-18 17:11:14 -0300

    Refactoring the Date on the Main class with LocalDate and creating the objects of the ContractService and OnlinePaymentService

--- application/Main.java
+++ application/Main.java
@@ -1,15 +1,19 @@
 package interfaces.application;
 
+import interfaces.entities.ContractService;
+import interfaces.entities.OnlinePaymentService;
+import interfaces.entities.PaypalService;
+
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
-
 
         System.out.println("Entre os dados do contrato:");
 
@@ -18,11 +22,8 @@ public class Main {
 
         System.out.print("Data (dd/MM/yyyy): ");
 
-        try {
-            Date date = dateFormat.parse(sc.next());
-        } catch (ParseException e) {
-            System.err.println("Formato incorreto!: " + e.getMessage());
-        }
+        LocalDate date = LocalDate.parse(sc.next(), DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+
 
         System.out.print("Valor do contrato: ");
         double contractValue = sc.nextDouble();
@@ -31,6 +32,10 @@ public class Main {
         int installments = sc.nextInt();
 
         System.out.println("Parcelas:");
+
+        OnlinePaymentService paymentService = new PaypalService();
+
+        ContractService service = new ContractService(paymentService);
 
         sc.close();
     }
